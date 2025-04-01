a877369f7136852cddd7fef39408de18c76af0b2
dhafinrazaqa <dhafin.razaqa12@gmail.com>
2025-04-01 15:33:31 +0700

    Menambahkan gambar pada tampilan about

--- app/src/main/java/com/dhafinrazaqa0030/asesmen1mobpro1/ui/screen/AboutScreen.kt
+++ app/src/main/java/com/dhafinrazaqa0030/asesmen1mobpro1/ui/screen/AboutScreen.kt
@@ -1,7 +1,15 @@
 package com.dhafinrazaqa0030.asesmen1mobpro1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -9,7 +17,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,9 +43,30 @@ fun AboutScreen() {
             )
         }
     ) { innerPadding ->
+        AboutScreenContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun AboutScreenContent(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = stringResource(R.string.logo),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.size(132.dp)
+        )
         Text(
             text = stringResource(R.string.copyright),
-            modifier = Modifier.padding(innerPadding).padding(16.dp)
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.fillMaxWidth()
         )
     }
 }
